62f8bab08b43bd3c9504ed4024bb5fd4377f7e6b
Roman Zimmermann <roman@more-onion.com>
2017-04-05 13:31:36 +0200

    Make A a controlled axis instead of using G33.

--- ellipse.py
+++ ellipse.py
@@ -1,19 +1,19 @@
 from math import pi, sin, sqrt
+import sys
 
 print("""g21
-g64
+g64 p1
 m8
 
-s26 m3
 g4p2
-f75
+f150
 """)
 
 z_length = 100.0  # (length)
 max_depth = 20.0  # (maximum depth)
 x_layer = 2.0     # (x-layer thickness)
 z_layer = 4.0     # (z-slice thickness)
-raster = 60       # (steps / commands per turn)
+raster = 90       # (steps / commands per turn)
 e_distance = 10.0  # Entry and exit distance.
 e_turns = 1       # Fixed amount of turns for an entry / exit move.
 jitter = 0.001
@@ -36,14 +36,37 @@ def find_max_x(z, min_x, form, raster):
     return mi, mx
 
 
-def g33(x, z, k):
-    assert k > 0
-    print("g33 x{} z{} k{}".format(round(x, 5), round(z, 5), round(k, 5)))
+class RasterMachine:
+    def __init__(self, raster):
+        self.raster = raster
+        self.x, self.z, self.a_step = 0, 0, 0
 
+    def gcoords(self):
+        a = float(self.a_step) / self.raster * 360
+        return "z{} x{} a{}".format(self.x, self.z, a)
+
+    def g0(self, x, z, a_step):
+        self.new_position(x, z, a_step)
+        print("g0 " + self.gcoords())
+
+    def g1(self, x, z, a_step):
+        self.new_position(x, z, a_step)
+        print("g1 " + self.gcoords())
+
+    def new_position(self, x, z, a_step):
+        self.x, self.z = x, z
+
+        turns, rel = divmod(self.a_step, self.raster)
+        a_rel = a_step % self.raster
+        if a_rel < rel:
+            turns += 1
+        self.a_step = turns * self.raster + a_rel
+
+
+m = RasterMachine(raster)
 
 lstep = 0
-for lstep in range(int(max_depth/x_layer) + 1):
-    print("g0 x{} z0".format(e_distance))
+for lstep in [int(max_depth/x_layer)]: #range(int(max_depth/x_layer) + 1):
     x = e_distance
     z = 0
     min_x = -x_layer * lstep
@@ -51,33 +74,25 @@ for lstep in range(int(max_depth/x_layer) + 1):
     # Do the entry-move.
     mi, mx = find_max_x(z, min_x, ellipse, raster)
     # Be at max x one raster step befor the actual max x.
-    mi = (mi-1) % raster
-    t = e_turns + float(mi) / raster
-    k = abs(x-mx) / t
+    m.g0(e_distance, z, mi-1)
     #print("Entry move in {} turns land at step {}".format(round(t, 2), mi))
-    g33(mx, z, k)
+    m.g1(mx, z, mi)
     x = mx
 
     # Complete the current turn.
     for phi_step in range(mi + 1, raster):
         phi = float(phi_step) / raster * 2 * pi
         nx = ellipse(z, phi, min_x)
-        if abs(nx - x) < epsilon:
-            nx += jitter
-        k = abs(nx - x) * raster
         #print(phi_step, ':', end='')
-        g33(nx, z, k)
+        m.g1(nx, z, phi_step)
         x = nx
 
     # Do another full turn.
     for phi_step in range(0, raster):
         phi = float(phi_step) / raster * 2 * pi
         nx = ellipse(z, phi, min_x)
-        if abs(nx - x) < epsilon:
-            nx += jitter
-        k = abs(nx - x) * raster
         #print(phi_step, ':', end='')
-        g33(nx, z, k)
+        m.g1(nx, z, phi_step)
         x = nx
 
     # Now do the spiral.
@@ -86,12 +101,8 @@ for lstep in range(int(max_depth/x_layer) + 1):
             phi = float(phi_step) / raster * 2 * pi
             nz = (zstep + float(phi_step) / raster) * z_layer
             nx = ellipse(nz, phi, min_x)
-            k = sqrt((nx - x)**2 + (nz - z)**2) * raster
-            if k < epsilon:
-                nx += jitter
-                k = sqrt((nx - x)**2 + (nz - z)**2) * raster
             #print(phi_step, ':', end='')
-            g33(nx, nz, k)
+            m.g1(nx, nz, phi_step)
             x = nx
             z = nz
     z = z_length
@@ -99,11 +110,8 @@ for lstep in range(int(max_depth/x_layer) + 1):
     for phi_step in range(0, raster):
         phi = float(phi_step) / raster * 2 * pi
         nx = ellipse(z, phi, min_x)
-        if abs(nx - x) < epsilon:
-            nx += jitter
-        k = abs(nx - x) * raster
         #print(phi_step, ':', end='')
-        g33(nx, z, k)
+        m.g1(nx, z, phi_step)
         x = nx
 
     # Move to the maximum x for this z.
@@ -111,14 +119,11 @@ for lstep in range(int(max_depth/x_layer) + 1):
     for phi_step in range(0, mi+1):
         phi = float(phi_step) / raster * 2 * pi
         nx = ellipse(z, phi, min_x)
-        if abs(nx - x) < epsilon:
-            nx += jitter
-        k = abs(nx - x) * raster
         #print(phi_step, ':', end='')
-        g33(nx, z, k)
+        m.g1(nx, z, phi_step)
         x = nx
 
     # Exit move.
-    print("g0 x{} z{}".format(e_distance, z))
+    m.g0(e_distance, z, phi_step)
 
 print("m2")
